b29eaa37523dea41992ac9dad080dbe815fe984d
Andrew Grogan-Kaylor <agrogan@umich.edu>
2023-01-22 10:49:57 -0500

    fixed up

--- plotly/plotly-map-test.R
+++ plotly/plotly-map-test.R
@@ -12,5 +12,5 @@ plot_ly(type = "scattermapbox") %>%
   layout(
     mapbox = list(
       style = 'open-street-map',
-      zoom =2.5,
-      center = list(lon = -88, lat = 34))) 
+      zoom =3.0,
+      center = list(lon = -83, lat = 42))) 
